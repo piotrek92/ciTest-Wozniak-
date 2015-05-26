8946225d5930f09831c6d28d966e8599629826de
piotrek92 <behi6@wp.pl>
2015-05-26 08:43:55 +0000

    newcommit

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var http = require('http');
 
 http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World\n');
+    response.writeHead(800, {'Content-Type': 'text/plain'});
+    response.end('Hello World new test\n');
 }).listen(8080);
 
 console.log('Server started');
